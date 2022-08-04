73e024a622cd9cbdcea22df47dd37fa228333c25
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-08-04 18:29:14 +0200

    balazs.matyassy: 4. task solved

--- src/main/java/hu/progmatic/blog/controller/BlogController.java
+++ src/main/java/hu/progmatic/blog/controller/BlogController.java
@@ -1,9 +1,12 @@
 package hu.progmatic.blog.controller;
 
+import hu.progmatic.blog.model.Entry;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import java.time.LocalDateTime;
 
 @Controller
 public class BlogController {
@@ -20,7 +23,12 @@ public class BlogController {
 
     @GetMapping("/bejegyzesek/{id}")
     public String viewEntry(@PathVariable Long id, Model model) {
-        model.addAttribute("id", id);
+        Entry entry = new Entry();
+        entry.setId(id);
+        entry.setTitle("Test Title");
+        entry.setContent("Test Content");
+        entry.setCreated(LocalDateTime.now());
+        model.addAttribute("entry", entry);
 
         return "entry.html";
     }
